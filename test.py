d2a09bbe1dc8491998e3df27c0e29fc3ecf68fbf
minhyeok <mahns1201@gmail.com>
2021-01-09 19:18:03 +0900

    [temp] test

--- test.py
+++ test.py
@@ -1,41 +1,10 @@
-import sys
+import pandas as pd
 
-##02 Defining a function for reading the daily climate data
-def readinput_Climatefile():
-    filename = "MET090.csv"
+df = pd.read_csv("MET090.csv", 'r', encoding="UTF8")
 
-    try:
-        filein = open(filename, 'r', encoding="UTF8")
-        line = filein.readline()
+print(df)
 
-        dailyclimate = []
-        # fmt = '%Y%m%d'
-        
-        while True:
-            line = filein.readline()
+header = list(df)
+header = header[0].split(',')
 
-            print(line)
-
-            if not line:
-                break
-            anslist = line.split(',')
-            temp = anslist[0]
-            anslist.append(temp)
- 
-            anslist[0] = temp[:8]    
-  
-            for n in range(1,len(anslist)-1):
-                anslist[n] = float(anslist[n])
-            dailyclimate.append(anslist)
-            
-        filein.close()
-        
-        # print(dailyclimate)
-
-        return dailyclimate
-    
-    except IOError:
-        print("***  파일 읽기 오류: 파일이 없거나 내용에 오류가 있습니다.")
-        sys.exit(1)
-
-readinput_Climatefile()
+print(header)
